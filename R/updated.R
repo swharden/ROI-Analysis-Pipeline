04732926facfdf15b0c013b6e0cbd5755505861a
beronicao <beronica.ocasio@gmail.com>
2017-05-31 15:48:27 -0400

    added graph features, changed output values to percentages

--- R/updated.R
+++ R/updated.R
@@ -80,7 +80,7 @@ Fb.df1 <- cbind(listofROIs2.df, Fb.df)
 
 df.m1 <- merge(df.m, Fb.df1, by="ROI")   # Adds 'Fb' column to the data
 
-df.m1$result <- Fbnorm.func(df.m1$Ft, df.m1$Fb)   # running Fb-normalization function (results are added as a new column of data labeled "result1")
+df.m1$result <- Fbnorm.func(df.m1$Ft, df.m1$Fb)   # running Fb-normalization function (results are added as a new column of data labeled "result")
 colnames(df.m1)[colnames(df.m1)=="result"] <- "dF.Fb"   #renaming column
 
 #############################
@@ -93,7 +93,7 @@ df.unmelted <- dcast(data = df.m2, formula = frame~ROI, fun.aggregate = sum, val
 adj.bROI.values = df.unmelted[,2, drop=FALSE]   # Keeps only "Mean1" column (baseline ROI values)
 df.m3 <- cbind(df.m2, adj.bROI.values)
 
-df.m3$result1 <- subtract.FbROI.func(df.m3$dF.Fb, df.m3$Mean1)   #running function (results are added as a new column of data labeled "result")
+df.m3$result1 <- subtract.FbROI.func(df.m3$dF.Fb, df.m3$Mean1)   #running function (results are added as a new column of data labeled "result1")
 colnames(df.m3)[colnames(df.m3)=="result1"] <- "dF.Fb.adj"   #renaming column of adjusted Ft values
 
 #############################
@@ -102,7 +102,11 @@ temp2a <- df.m3[ ,1:2, drop = FALSE]
 temp2b <- df.m3[ ,5, drop = FALSE]
 dF.Fb.values <- cbind(temp2a, temp2b)
 
-results_B <- dcast(data = dF.Fb.values, formula = frame~ROI, fun.aggregate = sum, value.var = "dF.Fb.adj")
+results_1 <- dcast(data = dF.Fb.values, formula = frame~ROI, fun.aggregate = sum, value.var = "dF.Fb.adj")
+
+results_2a <- results_1[,1, drop=FALSE]
+results_2b <- as.data.frame(apply(results_1[2:length(results_1)], 2, function(x) x*100))
+results_B <- cbind(results_2a, results_2b)
 
 write.table(results_B, file = "results_B.xls", row.names = FALSE, sep="\t", quote = FALSE) 
 cat("\nSAVED: ",normalizePath("results_B.xls"),"\n")
@@ -116,31 +120,35 @@ stdev.dF.F <- as.data.frame(apply(results_B[,3:length(results_B)], 1, sd))
 stats.dF.F <- cbind(results_B$frame, mean.dF.F, stdev.dF.F)
 colnames(stats.dF.F) <- c("frame", "mean.dF.F", "stdev.dF.F")
 
+graph.data <- cbind(results_B, stats.dF.F[,2, drop=FALSE])
+graph.data.m <- melt(graph.data, id.vars='frame')   #restructuring the data
+colnames(graph.data.m) <- c("frame", "ROI", "dF.F")  #renaming columns
+
 ####### Graphing Data #######
 
-rplot <- ggplot(data=dF.Fb.values, aes(x=dF.Fb.values[['frame']], y=(dF.Fb.values[['dF.Fb.adj']])*100, colour=ROI)) +
-  geom_point(size=0.02)
-rplot + labs(y = "dF/F (%)") +
+rplot1 <- ggplot(data=graph.data.m, aes(x=graph.data.m[['frame']], y=(graph.data.m[['dF.F']]), colour=ROI)) +
+  geom_line()
+rplot1 + labs(y = "dF/F (%)") +
+  labs(x = "frame") +
+  labs(title = expression(paste("GCaMP6f: Ca"^"2+"*" Activity")), subtitle = "ROI Traces") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(plot.subtitle = element_text(hjust = 0.5)) +
+  scale_x_continuous(expand = c(0.006,0)) +
+  png(filename = "fig_traces.png",
+    width = 1080, height = 720, units = "px", pointsize = 12)
+
+cat("\nSAVED: ",normalizePath("fig_traces.png"),"\n")
+##
+  
+rplot2 <- ggplot(data=stats.dF.F, aes(x=stats.dF.F[['frame']]))
+rplot2 + geom_ribbon(aes(ymin=stats.dF.F[['mean.dF.F']]-stats.dF.F[['stdev.dF.F']], ymax=stats.dF.F[['mean.dF.F']]+stats.dF.F[['stdev.dF.F']]), fill="grey", alpha=0.3) +
+  geom_line(aes(y=(stats.dF.F[['mean.dF.F']]))) + theme_bw() + 
+  labs(y = "dF/F (%)") +
   labs(x = "frame") +
   labs(title = expression(paste("GCaMP6f: Ca"^"2+"*" Activity"))) +
   theme(plot.title = element_text(hjust = 0.5)) +
   scale_x_continuous(expand = c(0.006,0)) +
-  png(filename = "fig_all.png",
-     width = 1080, height = 720, units = "px", pointsize = 12,
-     quality = 85)
+  png(filename = "fig_av.png", width = 1080, height = 720, units = "px", pointsize = 12)
 
-cat("\nSAVED: ",normalizePath("fig_all.png"),"\n")
-##
-
-rplot2 <- ggplot(data=stats.dF.F, aes(x=stats.dF.F[['frame']], y=(stats.dF.F[['mean.dF.F']])*100, colour=stats.dF.F[['mean.dF.F']])) +
-  geom_point(size=0.01) +
-  geom_errorbar(ymin= stats.dF.F$mean.dF.F-stats.dF.F$stdev.dF.F, ymax=stats.dF.F$mean.dF.F+stats.dF.F$stdev.dF.F) 
-
-rplot2 + labs(y = "dF/F (%)") +
-  labs(x = "frame") +
-  labs(title = expression(paste("GCaMP6f: Ca"^"2+"*" Activity"))) +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  png(filename = "fig_avg.png", width = 1080, height = 720, units = "px", pointsize = 12, quality = 85)
-
-cat("\nSAVED: ",normalizePath("fig_avg.png"),"\n")
+cat("\nSAVED: ",normalizePath("fig_av.png"),"\n")
 cat("\nDONE! \n")
