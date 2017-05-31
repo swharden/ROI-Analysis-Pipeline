bc13c437485d2bef774a5947d4edc0ab6d9b2cbf
beronicao <beronica.ocasio@gmail.com>
2017-05-31 11:48:44 -0400

    added setwd(), updated graphs

--- R/updated.R
+++ R/updated.R
@@ -1,19 +1,22 @@
 ####### INSTALL STUFF #######
 ##install.packages("readr")
-##install.packages("reshape2")
 ##install.packages("data.table")
+##install.packages("reshape2")
+##install.packages("ggplot2")
 
 ####### ROI Analysis ########
 library(utils)
-# Rscript --vanilla /GitHub/ROI-Analysis-Pipeline/R/updated.R
-#! usr/bin/env Rscript
-args <- commandArgs(TRUE)   
-
 library(readr)
 library(data.table)
+library(reshape2)
+library(ggplot2)
 
+# Rscript --vanilla /GitHub/ROI-Analysis-Pipeline/R/updated.R
+#! usr/bin/env Rscript
+args <- commandArgs(TRUE)  
+setwd(args[1])
 
-cat("\nLOADING: ",normalizePath("Results.xls"))
+cat("\nLOADING: ",normalizePath("Results.xls"),"\n")
 
 cell.dt <- read_tsv("Results.xls")
 colnames(cell.dt)[colnames(cell.dt)=="X1"] <- "frame"   #renaming column
@@ -23,11 +26,8 @@ frames = cell.dt[,1, drop=FALSE]   # Keeps only "frames" column
 
 #############################
 
-library(reshape2)
-
 df.m <- melt(cell.dt, id.vars='frame')   #restructuring the data
 colnames(df.m) <- c("frame", "ROI", "Ft")  #renaming columns
-head(df.m)
 
 #############################
 #### Creating functions #####
@@ -59,6 +59,7 @@ listofROIs2.df <- as.data.frame(listofROIs1)
 colnames(listofROIs2.df) <- c("ROI")
 
 #############################
+cat("\nLOADING: ",normalizePath("experiment.txt"),"\n")
 
 experiment <- read_lines("experiment.txt", skip=1)
 exp <- gsub("=", ",", experiment)
@@ -91,7 +92,6 @@ df.m3 <- cbind(df.m2, adj.bROI.values)
 
 df.m3$result1 <- subtract.FbROI.func(df.m3$dF.Fb, df.m3$Mean1)   #running function (results are added as a new column of data labeled "result")
 colnames(df.m3)[colnames(df.m3)=="result1"] <- "dF.Fb.adj"   #renaming column of adjusted Ft values
-head(df.m3)
 
 #############################
 
@@ -101,8 +101,8 @@ dF.Fb.values <- cbind(temp2a, temp2b)
 
 results_B <- dcast(data = dF.Fb.values, formula = frame~ROI, fun.aggregate = sum, value.var = "dF.Fb.adj")
 
-write.table(results_B, file = "results_B.xls", row.names = FALSE, sep="\t") 
-cat("\nSAVED: ",normalizePath("results_B.xls"))
+write.table(results_B, file = "results_B.xls", row.names = FALSE, sep="\t", quote = FALSE) 
+cat("\nSAVED: ",normalizePath("results_B.xls"),"\n")
 
 #############################
 
@@ -115,29 +115,29 @@ colnames(stats.dF.F) <- c("frame", "mean.dF.F", "stdev.dF.F")
 
 ####### Graphing Data #######
 
-library(ggplot2)
-# rplot <- ggplot(data=dF.Fb.values, aes(x=frame, y=(dF.Fb.values[['dF.Fb.adj']])*100, colour=ROI)) +
-#   geom_point(size=0.02) 
-# rplot + labs(y = "dF/F (%)") +
-#   labs(x = "frame") +
-#   labs(title = expression(paste("GCaMP6f: Ca"^"2+"*" Activity"))) +
-#   theme(plot.title = element_text(hjust = 0.5)) +
-#   scale_x_continuous(expand = c(0.006,0)) +
-#   jpeg(filename = "results_B.jpeg",
-#      width = 1080, height = 720, units = "px", pointsize = 12,
-#      quality = 75)
-
-###
-
-rplot2 <- ggplot(data=stats.dF.F, aes(x=stats.dF.F[['frame']], y=stats.dF.F[['mean.dF.F']], colour=stats.dF.F[['mean.dF.F']])) +
-  geom_point(size=0.01) +
-  geom_errorbar(ymin= stats.dF.F$mean.dF.F-stats.dF.F$stdev.dF.F, ymax=stats.dF.F$mean.dF.F+stats.dF.F$stdev.dF.F) 
-
-rplot2 + labs(y = "dF/F") +
+rplot <- ggplot(data=dF.Fb.values, aes(x=dF.Fb.values[['frame']], y=(dF.Fb.values[['dF.Fb.adj']])*100, colour=ROI)) +
+  geom_point(size=0.02)
+rplot + labs(y = "dF/F (%)") +
   labs(x = "frame") +
   labs(title = expression(paste("GCaMP6f: Ca"^"2+"*" Activity"))) +
   theme(plot.title = element_text(hjust = 0.5)) +
-  jpeg(filename = "results_B.jpeg", width = 1080, height = 720, units = "px", pointsize = 12, quality = 75)
+  scale_x_continuous(expand = c(0.006,0)) +
+  png(filename = "fig_all.png",
+     width = 1080, height = 720, units = "px", pointsize = 12,
+     quality = 85)
 
-cat("\nSAVED: ",normalizePath("results_B.jpeg"))
-cat("\nDONE!")
+cat("\nSAVED: ",normalizePath("fig_all.png"),"\n")
+##
+
+rplot2 <- ggplot(data=stats.dF.F, aes(x=stats.dF.F[['frame']], y=(stats.dF.F[['mean.dF.F']])*100, colour=stats.dF.F[['mean.dF.F']])) +
+  geom_point(size=0.01) +
+  geom_errorbar(ymin= stats.dF.F$mean.dF.F-stats.dF.F$stdev.dF.F, ymax=stats.dF.F$mean.dF.F+stats.dF.F$stdev.dF.F) 
+
+rplot2 + labs(y = "dF/F (%)") +
+  labs(x = "frame") +
+  labs(title = expression(paste("GCaMP6f: Ca"^"2+"*" Activity"))) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  png(filename = "fig_avg.png", width = 1080, height = 720, units = "px", pointsize = 12, quality = 85)
+
+cat("\nSAVED: ",normalizePath("fig_avg.png"),"\n")
+cat("\nDONE! \n")
