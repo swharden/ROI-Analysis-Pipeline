7357ac051087ed5f7454b3a0601738fe37b7e248
beronicao <beronica.ocasio@gmail.com>
2017-06-02 15:03:10 -0400

    fixed crash issue

--- R/updated.R
+++ R/updated.R
@@ -4,13 +4,13 @@
 ##install.packages("reshape2")
 ##install.packages("ggplot2")
 
-#system("Rscript --vanilla \\Spike\X_Drive\Data\SCOTT\2017-05-10 GCaMP6f\2017-05-10 GCaMP6f PFC OXTR cre\2017-06-01 cell2 Results.xls")
-
 # Rscript --vanilla /GitHub/ROI-Analysis-Pipeline/R/updated.R
+
 #! usr/bin/env Rscript
 args <- commandArgs(TRUE)  
 setwd(args[1])
 wd.name <- basename(getwd())
+
 res.pref = 300  # set graph resolution (ppi)
 
 ####### ROI Analysis ########
@@ -30,28 +30,38 @@ colnames(cell.dt)[1] <- "frame"
 ROImeans.dt <- cell.dt[,2:length(cell.dt), drop=FALSE]   # Keeps only "Mean_" columns (ROI mean values)
 actual.frames = cell.dt[,1, drop=FALSE]   # Keeps only "frames" column
 
-
 fnames <- Sys.glob("*.tif")
 fnames1 <- gsub(".tif", "", fnames, fixed = TRUE)
 fnames2 <- as.double(fnames1)
-t0 <- (fnames2[1])-((fnames2[3])-(fnames2[2]))
 
-fnames2[is.na(fnames2)] <- t0 
-fnames3 <- sort(fnames2)
-fnames3.df <- as.data.frame(fnames3)
-colnames(fnames3.df)[1]<-"time"
+if (sum(is.na(fnames2))<2){
+  
+t0 <- (fnames2[1])-((fnames2[3])-(fnames2[2]));
+fnames2[is.na(fnames2)] <- t0; 
+fnames3 <- sort(fnames2);
+fnames3.df <- as.data.frame(fnames3);
+colnames(fnames3.df)[1]<-"time";
 
-subtract.funct <- function(x) {(x-t0)/60}
-fnames.df <- as.data.frame(subtract.funct(fnames3.df))
-frame.time <- cbind(actual.frames, fnames.df)
+mins.funct <- function(x) {(x-t0)/60};
+fnames.df <- as.data.frame(mins.funct(fnames3.df));
 
-if (anyDuplicated(fnames.df)==0) +
-  if (is.numeric(fnames.df[1:length(fnames.df),])==TRUE) + 
-  if (nrow(fnames.df)==nrow(cell.dt)){
-       cell.dt<-cbind(fnames.df[,1, drop=FALSE], cell.dt[,2:length(cell.dt)])} else{
-         cat("\nCheck tiff file names. Frame times read from file names in the format \"[time].tif\" (e.g. 149177004.547.tif.) Proceeding with frame number instead of time.\n")}
+if (anyDuplicated(fnames.df)==0 &&
+  is.numeric(fnames.df[1:length(fnames.df),])==TRUE && 
+  nrow(fnames.df)==nrow(cell.dt)){
+       cell.dt<-cbind(fnames.df[,1, drop=FALSE], cell.dt[,2:length(cell.dt)]);
+       time.unit = "Experiment Duration (minutes)"} else{
+         cat("\nCheck tiff file names. Frame times read from file names in the format \"[time].tif\" (e.g. 149177004.547.tif.) Proceeding with frame number instead of time.\n");
+         fnames.df <- actual.frames;
+         cat(head(fnames.df));
+         time.unit = "Experiment Duration (frames)"};
+} else{fnames.df <- actual.frames;
+  time.unit = "Experiment Duration (frames)";
+  cat("\nCheck tiff file names. Unable to use frame times. Proceeding with frame number instead of time.\n")}
 
 ###
+colnames(fnames.df)[1]<-"time"
+frame.time <- cbind(actual.frames, fnames.df);
+
 colnames(cell.dt)[1] <- "frame" 
 frames = cell.dt[,1, drop=FALSE]   # Keeps only "frames" column
 
@@ -93,17 +103,36 @@ colnames(listofROIs2.df) <- c("ROI")
 cat("\nLOADING: ",normalizePath("experiment.txt"),"\n")
 
 experiment.info <- read_lines("experiment.txt")
-exp.info <- experiment.info[1]
+exp.info1 <- experiment.info[1]
+exp.info <- gsub("#","", exp.info1)
 
 experiment <- read_lines("experiment.txt", skip=1)
 exp <- gsub("=", ",", experiment)
 exp1 <- gsub("-", ",", exp)
 exp.values <- read.table(textConnection(exp1), sep = ",", row.names = 1, col.names = c("condition","b1","b2"))
+exp.val1 <- read.table(textConnection(exp1), sep = ",", row.names = NULL, col.names = c("condition","b1","b2"))
+exp.val2 <- as.matrix(exp.val1)
 
-b.range <- exp.values["baseline",1:2]
-TGOT.range <- exp.values["TGOT",1:2]
+exp.b <- grep("baseline", exp.val2, ignore.case = TRUE)
+exp.t <- grep("TGOT", exp.val2, ignore.case = TRUE)
+
+b.range <- exp.values[as.numeric(paste(exp.b)),1:2]
+TGOT.range <- exp.values[as.numeric(paste(exp.t)),1:2]
 cond.range.df <- rbind(b.range, TGOT.range)
+#cr.df <- cond.range.df
+ft.df <- frame.time
 
+ft.b1 <- ft.df[ft.df$frame==(b.range[,1]),]
+ft.b2 <- ft.df[ft.df$frame==(b.range[,2]),]
+ft.t1 <- ft.df[ft.df$frame==(TGOT.range[,1]),]
+ft.t2 <- ft.df[ft.df$frame==(TGOT.range[,2]),]
+
+b.xmin=ft.b1[['time']] 
+b.xmax=ft.b2[['time']]
+t.xmin=ft.t1[['time']]
+t.xmax=ft.t2[['time']]
+
+# # # # # #
 Fb.range <- cell.dt[(b.range[[1]]:b.range[[2]]), , drop=FALSE]
 Fb.values <- Fb.range[ ,2:length(Fb.range), drop=FALSE]
 Fb.values.mat <- as.matrix(Fb.values)
@@ -163,20 +192,6 @@ traces.data <- cbind(stats.dF.F[,1:2, drop=FALSE], traces.data1[,2:length(traces
 traces.data.m <- melt(traces.data, id.vars='frame')
 colnames(traces.data.m) <- c("frame", "ROI", "dF.F")
 
-####
-
-cr.df <- cond.range.df
-ft.df <- frame.time
-
-ft.b1 <- ft.df[ft.df$frame==(cr.df[['baseline',1]]),]
-ft.b2 <- ft.df[ft.df$frame==(cr.df[['baseline',2]]),]
-ft.t1 <- ft.df[ft.df$frame==(cr.df[['TGOT',1]]),]
-ft.t2 <- ft.df[ft.df$frame==(cr.df[['TGOT',2]]),]
-
-b.xmin=ft.b1[['time']] 
-b.xmax=ft.b2[['time']]
-t.xmin=ft.t1[['time']]
-t.xmax=ft.t2[['time']]
 
 ####### Graphing Data #######
 
@@ -186,7 +201,7 @@ rplot1 <- ggplot(data=traces.data.m, aes(x=traces.data.m[['frame']], y=(traces.d
   geom_line(aes(colour=ROI, alpha=0.07), show.legend = FALSE) 
 
 rplot1 + labs(y = "dF/F (%)") +
-  labs(x = "Experiment Duration (min)") +
+  labs(x = paste(time.unit)) + 
   labs(title = expression(paste("GCaMP6f: Ca"^"2+"*" Activity-- ROI Traces")), subtitle = paste(wd.name, exp.info)) +
   theme(plot.title = element_text(hjust = 0.5)) +
   theme(plot.subtitle = element_text(hjust = 0.5)) +
@@ -204,7 +219,7 @@ rplot2 <- ggplot(data=stats.dF.F, aes(x=stats.dF.F[['frame']])) +
 rplot2 + geom_ribbon(aes(ymin=stats.dF.F[['mean.dF.F']]-stats.dF.F[['stdev.dF.F']], ymax=stats.dF.F[['mean.dF.F']]+stats.dF.F[['stdev.dF.F']]), fill="grey", alpha=0.3) +
   geom_line(aes(y=(stats.dF.F[['mean.dF.F']]))) + theme_bw() + 
   labs(y = "dF/F (%)") +
-  labs(x = "Experiment Duration (min)") +
+  labs(x = paste(time.unit)) +
   labs(title = expression(paste("GCaMP6f: Ca"^"2+"*" Activity-- ROI Average")), subtitle = paste(wd.name, exp.info)) +
   theme(plot.title = element_text(hjust = 0.5)) +
   theme(plot.subtitle = element_text(hjust = 0.5)) +
