b4badec611ddae1289c37edb3a10e876170f720d
beronicao <beronica.ocasio@gmail.com>
2017-06-08 13:55:23 -0400

    Added package documentation

---
+++ boshROI/R/boshROI-package.R
@@ -0,0 +1,10 @@
+#' boshROI
+#'
+#' A package to assist in the quantification and analysis of fluorescence micrographs.
+#'
+#' @docType package
+#' @name boshROI
+#' @keywords package
+#' @import methods
+#' @useDynLib boshROI
+NULL
