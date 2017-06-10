ffb9f49bd3512b24af44ae785925deaeda094535
Scott W Harden <swharden@gmail.com>
2017-06-10 18:11:21 -0400

    pyLS batch mode and index file generation

--- pyLS/processFolders.py
+++ pyLS/processFolders.py
@@ -2,8 +2,10 @@ from pyLineScan import LineScan
 import glob
 import os
 from PIL import Image
+import matplotlib.pyplot as plt
+import datetime
 
-def analyzeSubfolders(folderParent):
+def analyzeSubfolders(folderParent,overwrite=False):
     """
     given a parent directly, perform automated linescan analysis on all sub-folders.
     Output data is saved in each linescan folder's 'results' sub-folder.
@@ -12,22 +14,55 @@ def analyzeSubfolders(folderParent):
     print("analyzing all linescans in",folderParent)
     linescanFolders=sorted(os.listdir(folderParent))
     for i,name in enumerate(linescanFolders):
+        if not name.startswith("LineScan-"):
+            continue
         folderLinescan=os.path.join(folderParent,name)
         print("PROCESSING LINESCAN %d OF %d: %s"%(i+1,len(linescanFolders),name))
         folderOutput=os.path.join(folderLinescan,"analysis")
         if not os.path.exists(folderOutput):
             os.mkdir(folderOutput)
-        if not os.path.exists(os.path.join(folderOutput,"fig_swh_dual.png")):
+        if overwrite or not os.path.exists(os.path.join(folderOutput,"fig_swh_dual.png")):
             print(" analyzing linescan data...")
             LS=LineScan(folderLinescan)
             LS.figureDual(os.path.join(folderOutput,"fig_swh_dual.png"))
-        if not os.path.exists(os.path.join(folderOutput,"ref.png")):
+            plt.close('all')
+        if overwrite or not os.path.exists(os.path.join(folderOutput,"ref.png")):
             refFigures=glob.glob(folderLinescan+"/References/*Window2*.tif")
             if len(refFigures):
                 print(" generating reference figure...")
                 im=Image.open(refFigures[0])
                 im.save(os.path.join(folderOutput,"ref.png"))
 
+def index(folderParent):
+    """make index.html and stick it in the parent directory."""
+    timestamp=datetime.datetime.now().strftime("%I:%M %p on %B %d, %Y")
+    folders=os.listdir(folderParent)
+    out="<html><body>"
+    out+="<b style='font-size: 300%%'>boshLS</b><br><i>automatic linescan index generated at %s</i><hr><br>"%timestamp
+    for folder in sorted(folders):
+        if not folder.startswith("LineScan-"):
+            continue
+        path=os.path.abspath(folderParent+"/"+folder)
+        rel=folderParent+"/"+folder
+        out+="<div style='background-color: #336699; color: white; padding: 10px; page-break-before: always;'>"
+        out+="<span style='font-size: 200%%; font-weight: bold;'>%s</span><br>"%folder
+        out+="<code>%s</code></div>"%path
+        out+="<table cellpadding=20><tr>"
+        for fname in ['ref.png','fig_swh_dual.png']:
+            out+='<td valign="top">'
+            out+='<a href="%s/analysis/%s"><img src="%s/analysis/%s"></a>'%(rel,fname,rel,fname)
+            out+='</td>'
+        out+="</tr></table>"
+    out+="</code></body></html>"
+    fileOut=os.path.abspath(folderParent+"/index.html")
+    with open(fileOut,'w') as f:
+        f.write(out)
+    print("saved",fileOut)
+
+
 if __name__=="__main__":
-    analyzeSubfolders('../data/linescan/realistic/')
+    #folderParent='../data/linescan/realistic/'
+    folderParent=r'X:\Data\SCOTT\2017-06-08 2p F5 tests\2017-06-09 F4vF5\2p'
+    analyzeSubfolders(folderParent,overwrite=True)
+    index(folderParent)
     print("DONE")
--- pyLS/pyLineScan.py
+++ pyLS/pyLineScan.py
@@ -1,19 +1,19 @@
-# pyLineScan is a package to aid analysis of two-photon linescans 
+# pyLineScan is a package to aid analysis of two-photon linescans
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 
 DELTA=r'$\Delta$'
-    
+
 class LineScan:
-    def __init__(self,folder,verbose=False,baseline=[1.5,2.25]):
+    def __init__(self,folder,verbose=False,baseline=[.0,.5]):
         """
         The LineScan class provides an easy object to load and analyze data from PrairieView linescan folders.
         By convension Ch1 is red (calcuim insensitive fluorophore) and Ch2 is green (calcium indicator).
         The main objects to access are green (G), red (R), and green over red (GoR).
         Baseline inputs (in seconds) are used to convert G/R to Delta(G/R)
         """
-        
+
         # with out the path and name of the linescan
         self.folder=os.path.abspath(folder)
         self.verbose=verbose
@@ -21,7 +21,7 @@ class LineScan:
         assert(os.path.exists(self.folder)), self.folder+" doesn't exist"
         self.name=os.path.basename(self.folder)
         if verbose: print("loading linescan",self.name)
-        
+
         # figure out which files are linescans, XML data, etc
         self.files=sorted(os.listdir(self.folder))
         assert len([x for x in self.files if x.endswith(".env")]), "no .env file found"
@@ -38,7 +38,7 @@ class LineScan:
         self.confLoad() # automatically load the configuration and do the analysis
         self.dataLoad() # leaves data as 2D arrays (images)
         self.dataFlatten() # converts data to 1D arrays (traces)
-        
+
     def _xml_getValue(self,s):
         """return the value from an XML line ('<PVStateValue key="dwellTime" value="7.2" />' becomes 7.2)"""
         s=s.split("value=")[1].split('"')[1]
@@ -65,7 +65,7 @@ class LineScan:
         if self.baselineSec:
             self.baselineIs=[int(self.baselineSec[0]/self.conf['scanLinePeriod']),
                              int(self.baselineSec[1]/self.conf['scanLinePeriod'])]
-        
+
     def dataLoad(self):
         """load TIF data as a 2d array and store it in the lists self.dataG and self.dataR"""
         self.dataR,self.dataG,self.dataGoR=[None]*self.frames,[None]*self.frames,[None]*self.frames
@@ -73,7 +73,7 @@ class LineScan:
             self.dataR[frame]=plt.imread(os.path.join(self.folder,self.filesR[frame]))
             self.dataG[frame]=plt.imread(os.path.join(self.folder,self.filesG[frame]))
             self.dataGoR[frame]=self.dataG[frame]/self.dataR[frame]
-    
+
     def dataFlatten(self):
         """Flatten 2d data into 1d data. Creates traceG, traceR, and traceGoR."""
         #TODO: this is where alternative trace generation methods may be used (i.e., just use brightest 10% of pixels)
@@ -86,13 +86,13 @@ class LineScan:
             if self.baselineSec:
                 self.baselines[frame]=np.average(self.traceGoR[frame][self.baselineIs[0]:self.baselineIs[1]])
                 self.traceGoR[frame]=self.traceGoR[frame]-self.baselines[frame]
-                
+
     def figureRaw(self,saveAs=False):
         """generate a basic summary figure of the analyzed data."""
         plt.figure(figsize=(4,3))
         plt.title("Raw PMT Data",fontsize=16)
         plt.ylabel("raw pixel intensity (AFU)")
-        plt.xlabel("linescan duration (seconds)")        
+        plt.xlabel("linescan duration (seconds)")
         plt.grid(alpha=.5)
         for frame in range(self.frames):
             plt.plot(self.Xs,self.traceG[frame],'-',color='G',alpha=.5)
@@ -100,15 +100,15 @@ class LineScan:
         plt.margins(0,.1)
         plt.axis([None,None,0,None])
         plt.tight_layout()
-        if saveAs: 
+        if saveAs:
             plt.savefig(saveAs,dpi=self.dpi)
-                    
+
     def figureGoR(self,saveAs=False):
         """generate a basic summary figure of the analyzed data."""
         plt.figure(figsize=(4,3))
         plt.title("Raw PMT Data",fontsize=16)
         plt.ylabel(DELTA+"[G/R] (%)")
-        plt.xlabel("linescan duration (seconds)")        
+        plt.xlabel("linescan duration (seconds)")
         plt.grid(alpha=.5)
         plt.axhline(0,color='k',ls='--')
         plt.axvspan(self.baselineSec[0],self.baselineSec[1],alpha=.1,color='k')
@@ -116,15 +116,15 @@ class LineScan:
             plt.plot(self.Xs,self.traceGoR[frame]*100,'-',color='b',alpha=.5)
         plt.margins(0,.1)
         plt.tight_layout()
-        if saveAs: 
+        if saveAs:
             plt.savefig(saveAs,dpi=self.dpi)
-        
+
     def figureDual(self,saveAs=False):
         plt.figure(figsize=(6,10))
-        
+
         ax1=plt.subplot(411)
         plt.title(self.name)
-        plt.ylabel("raw pixel intensity (AFU)")       
+        plt.ylabel("raw pixel intensity (AFU)")
         plt.grid(alpha=.5)
         for frame in range(self.frames):
             plt.plot(self.Xs,self.traceG[frame],'-',color='G',alpha=.5)
@@ -132,9 +132,9 @@ class LineScan:
         plt.margins(0,.1)
         plt.axis([None,None,0,None])
         plt.setp(plt.gca().get_xticklabels(), visible=False)
-        
+
         plt.subplot(412,sharex=ax1)
-        plt.ylabel(DELTA+" [G/R] (%)")     
+        plt.ylabel(DELTA+" [G/R] (%)")
         plt.grid(alpha=.5)
         plt.axhline(0,color='k',ls='--')
         plt.axvspan(self.baselineSec[0],self.baselineSec[1],alpha=.1,color='k')
@@ -142,22 +142,22 @@ class LineScan:
             plt.plot(self.Xs,self.traceGoR[frame]*100,'-',color='b',alpha=.5)
         plt.margins(0,.1)
         plt.setp(plt.gca().get_xticklabels(), visible=False)
-        
+
         plt.subplot(413,sharex=ax1)
         plt.imshow(np.rot90(np.average(self.dataG,axis=0)),cmap='gray',aspect='auto',extent=plt.axis())
         plt.setp(plt.gca().get_yticklabels(), visible=False)
         plt.setp(plt.gca().get_xticklabels(), visible=False)
         plt.ylabel("green channel")
-        
+
         plt.subplot(414,sharex=ax1)
         plt.imshow(np.rot90(np.average(self.dataR,axis=0)),cmap='gray',aspect='auto',extent=plt.axis())
         plt.setp(plt.gca().get_yticklabels(), visible=False)
         plt.ylabel("red channel")
-        
-        plt.xlabel("linescan duration (seconds)")   
-        
-        plt.tight_layout()     
-        if saveAs: 
+
+        plt.xlabel("linescan duration (seconds)")
+
+        plt.tight_layout()
+        if saveAs:
             plt.savefig(saveAs,dpi=self.dpi)
 
 if __name__=="__main__":
