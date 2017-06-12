8ebbcff1a5ca3de1513a4a16e4ae000359cd7d17
beronicao <beronica.ocasio@gmail.com>
2017-06-12 14:41:49 -0400

    edited linescans.R

--- sandbox/beronica/linescans.R
+++ sandbox/beronica/linescans.R
@@ -10,10 +10,7 @@ for(fileN in 1:length(fnames)){
   assign(fnames1[fileN],readTIFF(paste(fnames[fileN]), info = TRUE, as.is = TRUE))
 }
 
-rowmeans.names<-matrix(ncol=length(fnames1))
-rowmeans.names<-matrix()
 rowmeans.names<-dim(NULL)
-
 for (x in fnames1[1:length(fnames1)]){
   #rowmeans.names <- data.frame(1:length(fnames1))
     xdf<- get(x, envir = .GlobalEnv);
