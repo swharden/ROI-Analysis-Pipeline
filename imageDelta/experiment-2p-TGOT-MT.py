ca948e6724b7674eb93e54ee0d8267c16cc415e0
Scott W Harden <swharden@gmail.com>
2019-02-14 09:29:27 -0500

    add notes for LED vs 2P

--- imageDelta/experiment-2p-TGOT-MT.py
+++ imageDelta/experiment-2p-TGOT-MT.py
@@ -6,10 +6,16 @@ import os
 import imageDelta as id
 
 if __name__ == "__main__":
-    folderPath = R"X:\Data\OT-Cre\OT-GCaMP intrinsic\190128-821693\TSeries-01282019-1332-1404"
+    folderPath = R"X:\Data\OTR-Cre\GCaMP6f PFC injection patch and linescan\2019-02-13\slice2\TSeries-02132019-1317-1418"
     print(f"\n\nANALYZING: {folderPath}")
     drugs = []
-    drugs.append(id.Drug("TGOT", id.drugFrameFromFileList(5, folderPath), durationFrames=60*3, padFrames=30))
-    drugs.append(id.Drug("MT", id.drugFrameFromFileList(15, folderPath), durationFrames=60*3, padFrames=30))
+
+    # use frame numbers when working with two-photon time series
+    drugs.append(id.Drug("TGOT", 98, durationFrames=20, padFrames=5))
+
+    # use time-encoded filenames when dealing with LED images
+    #drugs.append(id.Drug("TGOT", id.drugFrameFromFileList(10, folderPath), durationFrames=60*3, padFrames=30))
+    #drugs.append(id.Drug("MT", id.drugFrameFromFileList(20, folderPath), durationFrames=60*3, padFrames=30))
+
     id.graphDrugExperiment(drugs, folderPath)
     print("DONE")
